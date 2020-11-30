cb5b9ad7075481403790e0741c971a90d89cf991
vijay <nimmagaddav9@gmail.com>
2020-11-30 12:17:35 -0800

    initial commit

---
+++ demo.js
@@ -0,0 +1,30 @@
+/*
+var sub: string = "Angular10";
+// we can represnt the string by using ``
+//`` backtick introduced in ES6
+var wish: string = `Welcome to ${sub}`;
+
+console.log(wish);
+
+*/
+/*
+var table_name: string = "employees";
+
+var sal: number = 50000;
+
+var sql_query: string = `select * from ${table_name} where esal>${sal}`;
+
+console.log(sql_query); //select * from employees where esal>50000
+
+*/
+/*
+var decimalNum: number = 100;
+var doubleNum: number = 100.12345;
+var hexadecimalNum: number = 0x123abc;
+var octaNum: number = 0o123;
+var binaryNum: number = 0b1010;
+console.log(decimalNum, doubleNum, hexadecimalNum, octaNum, binaryNum);//100 100.12345 1194684 83 10
+
+*/
+var flag = true;
+console.log(flag);
